309076a2c74cbcdc3a547b83b51e88cb0aa998c1
andrei-baton <andrei@batoncorporation.com>
2025-12-05 17:00:00 -0800

    refactor: export all havoc modules from main index

--- src/havoc/index.ts
+++ src/havoc/index.ts
@@ -10,3 +10,7 @@ export { HavocOrchestrator, type HavocConfig, type HavocStatus } from "./havoc-c
 export { default as CreatorReputationIndex, type CRIBand } from "./cri";
 export { default as HavocMarketMaker, type PoolState, type MayhemSignal, type MMAction } from "./market-maker";
 export { default as HavocStateMachine, type HavocState, type StateTransition } from "./state-machine";
+export { default as MayhemSignalProvider, type MayhemSignalData, type ChaosMetrics } from "./mayhem-signal-provider";
+export { default as LiquidityManager, type LiquidityOrder, type LiquidityBudget } from "./liquidity-manager";
+export { default as PoolIntegration, type HavocPoolState } from "./pool-integration";
+export { default as PerformanceDashboard, type PerformanceMetrics, type DashboardSnapshot } from "./performance-dashboard";
